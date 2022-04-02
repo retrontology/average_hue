5b17f067cd7d74ecb049897bccf06d91a52570ce
Retrontology <retrontology@hotmail.com>
2022-04-02 00:29:08 -0500

    pulse

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,62 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::{thread, time};
 use hueclient::{Bridge, CommandLight};
 use text_io::read;
 use std::path::Path;
 use uuid::Uuid;
 
 const UUIDFILENAME: &str = "bridge.uuid";
+const PERIOD: u16 = 1;
+const COLOUR: u16 = 25500;
+const OFF: bool = true;
 
 fn main() {
-    let bridge = get_bridge(Path::new(UUIDFILENAME)); 
-    println!("the username was {}", bridge.username);
-    let light_command = CommandLight::default().on();
-    for light in &bridge.get_all_lights().unwrap() {
-        bridge.set_light_state(light.id, &light_command).unwrap();
+    let bridge = get_bridge(Path::new(UUIDFILENAME));
+    println!("the username was {}", bridge.username); 
+    let lights = &bridge.get_all_lights().unwrap();
+    let wait_time: u16 = PERIOD/2;
+    let transition_time: u16 = PERIOD / 2 * 10;
+    if OFF {
+        let light_command = CommandLight::default().off();
+        for light in lights {
+            bridge.set_light_state(light.id, &light_command).unwrap();
+        }
+    } else {
+        loop {
+            let mut light_command = CommandLight {
+                on: Some(true),
+                bri: Some(254),
+                hue: Some(COLOUR),
+                sat: Some(254),
+                ct: None,
+                xy: None,
+                transitiontime: Some(transition_time),
+                alert: None,
+                scene: None,
+            };
+            for light in lights {
+                bridge.set_light_state(light.id, &light_command).unwrap();
+            }
+            thread::sleep(time::Duration::from_secs(wait_time.into()));
+            light_command = CommandLight {
+                on: Some(true),
+                bri: Some(0),
+                hue: Some(COLOUR),
+                sat: Some(254),
+                ct: None,
+                xy: None,
+                transitiontime: Some(transition_time),
+                alert: None,
+                scene: None,
+            };
+            for light in lights {
+                bridge.set_light_state(light.id, &light_command).unwrap();
+            }
+            thread::sleep(time::Duration::from_secs(wait_time.into()));
+        }
     }
+    
 }
 
 fn get_bridge(uuid_path: &Path) -> Bridge{
